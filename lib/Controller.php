bf322e1fe6deb13107463915b0f1c004c7e4366a
satch <andre@rihani.fr>
2015-01-23 22:37:23 +0100

    first import

---
+++ lib/Controller.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * le but de cette classe est degérer l'aacès au site en décortiquant l'URL
+ */
+
+namespace YAPF\Controller;
+
+/**
+ * Description of Controller
+ *
+ * @author satch
+ */
+abstract class Controller {
+
+    public static function run() {
+
+    }
+
+    protected static function _loadConfig() {
+
+    }
+
+}
